6efaa98cdd4d0460064c1345bab4fcb127ec82fe
Amanda King <amapickell@gmail.com>
2019-05-31 00:40:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,5 +41,32 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  
+  total = 0 
+  cart = consolidate_cart(cart)
+
+  if cart.length == 1 
+    cart = apply_coupons(cart, coupon)
+    cart_discount = apply_clearance(cart)
+    if cart.length > 1 
+      cart_discount.map do |item, price_hash|
+        if price_hash[:count] >= 1 
+          total += (price_hash[:price] * price_hash[:count])
+        end
+      end
+    else 
+      cart_discount.map do |item, price_hash|
+        total += (price_hash[:price] * price_hash[:count])
+      end
+    end
+  else 
+    cart = apply_coupons(cart, coupon)
+    cart_discount = apply_clearance(cart)
+    cart_discount.map do |item, price_hash|
+      total += (price_hash[:price] * price_hash[:count])
+    end
+  end
+  if total > 100
+    total = total * (0.9) 
+  end
+total
 end
